6fead3170aa45de41a8c7ed14e1a06a3b54f2659
Swaybee <swaybeegames@gmail.com>
2026-01-12 13:27:14 +0100

    feat: fonction switch_dim du player

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
-
+class_name Player
 
 const SPEED = 300.0
 
+signal switch_dim(player: Player)
+
+enum Dimension {A, B, C}
+
+@export var unique_dimension: Dimension
+@export var common_dimension: Dimension
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -12,3 +18,6 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func swicth_dim():
+	emit_signal("switch_dim", [self])

c57fd618964dacf3461e60001d022ceeb9075526
IlanRigolio <rigolioilan@gmail.com>
2026-01-15 23:40:51 +0100

    ajout detection clef et coffre + message

--- game/scripts/clef.gd
+++ game/scripts/clef.gd
@@ -6,6 +6,7 @@ class_name Clef
 const clef_A = "res://assets/coffre/clef_bleue.png"
 const clef_B = "res://assets/coffre/clef_verte.png"
 const clef_C = "res://assets/coffre/clef_rouge.png"
+@onready var zone_detection = $Detection
 
 func _ready() -> void:
 	match dim:
@@ -26,6 +27,9 @@ func _ready() -> void:
 			$Sprite2D.texture = load(clef_C)
 
 	body_entered.connect(_on_body_entered)
+	zone_detection.collision_layer = 7
+	zone_detection.collision_mask = 7
+	zone_detection.body_entered.connect(_on_zone_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -33,3 +37,9 @@ func _on_body_entered(body: Node2D) -> void:
 		if succes:
 			print("Clef ramassée : " + key_id)
 			queue_free()
+			
+func _on_zone_entered(body: Node2D) -> void:
+	if body is Player:
+		if not body.is_in_group(dim):
+			body.show_message("Il semble y avoir quelque chose ici ...")
+			return

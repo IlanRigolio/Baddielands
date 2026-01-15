0df66492f84312865d9347665f20d382650de522
thor0020 <charles.thorin@etudiant.univ-reims.fr>
2026-01-15 15:12:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
+printf '\033c\033]0;%s\a' GJ4
+base_path="$(dirname "$(realpath "$0")")"
+"$base_path/game/Baddielands.x86_64" "$@"
 
-ORIGINAL_DIR="$(pwd)"
-trap 'cd "$ORIGINAL_DIR"' EXIT
-
-PROJECT_ROOT="$(cd "$(dirname "$0")" && pwd)"
-cd "$PROJECT_ROOT/game"
-
-uv run main.py

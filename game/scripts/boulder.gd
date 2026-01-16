59074674861ecd28d6c41339a74aedc68d63bc19
Swaybee <swaybeegames@gmail.com>
2026-01-16 04:41:20 +0100

    énigmes du labyrinthe et des trous

--- game/scripts/boulder.gd
+++ game/scripts/boulder.gd
@@ -4,10 +4,21 @@ class_name Boulder
 @export var id: int = 0
 @export var dest: Vector2 = global_position
 
+@onready var origine: Vector2 = global_position
+
 var SPEED = 200.0
-var has_moved: bool = false
+var activated_plates: int:
+	set(value):
+		activated_plates = value
+		if activated_plates == 0:
+			come_back()
+		else:
+			move()
 
 func move():
-	has_moved = true
 	var tween = create_tween()
-	tween.tween_property(self, "global_position", dest, Vector2(global_position - dest).length() / SPEED)
+	tween.tween_property(self, "global_position", dest, Vector2(origine - dest).length() / SPEED)
+
+func come_back():
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", origine, Vector2(dest - origine).length() / SPEED)
